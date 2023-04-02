58879fdea330d45708929e26c77932b1dea6166d
letieu <letieu8@gmail.com>
2023-04-02 22:28:16 +0700

    feat: add follow current file feature

--- plugin/hacker.lua
+++ plugin/hacker.lua
@@ -1,1 +1,5 @@
 vim.api.nvim_create_user_command("Hack", require("hacker").start, {})
+
+-- call start(true)
+vim.api.nvim_create_user_command("HackFollow", function() require("hacker").start(true) end, {})
+
